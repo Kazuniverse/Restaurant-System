d302eac55d1276d2e36ff5cce4355b71b477c3d0
Kazuniverse <khafidzr05@gmail.com>
2025-09-26 21:16:15 +0700

    Add project files.

---
+++ Page/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Restaurant_System.Page;
+
+namespace Restaurant_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            LoadPage(new TableSeating());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoadPage(new Page.Menu());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LoadPage(new History());
+        }
+        public void LoadPage(UserControl halaman)
+        {
+            panelContainer.Controls.Clear();
+            Dock = DockStyle.Fill;
+            panelContainer.Controls.Add(halaman);
+        }
+    }
+}
---
+++ Page/TableSeating.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurant_System.Page
+{
+    public partial class TableSeating : UserControl
+    {
+        public TableSeating()
+        {
+            InitializeComponent();
+        }
+    }
+}
